9a1d4223470bab817bb3d2b26483db00bd96ad7e
git-Sahitya <sahitya5289@gmail.com>
2025-07-10 19:13:12 +0530

    Implement database connection with Mongoose and update package dependencies

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,16 @@
-const express = require("express")
-const cookieParser = require("cookie-parser")
-const cors = require("cors")
-const dotenv = require("dotenv")
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
-const app = express()
-dotenv.config()
+const app = express();
+dotenv.config();
+PORT = process.env.PORT || 5000;
 
-PORT = process.env.PORT || 5000
+//Database connection
+connectDB();
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running on PORT ${PORT}`);
-    
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}`);
+});
